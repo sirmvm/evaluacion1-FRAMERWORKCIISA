5e377d05181a926df276266b035a8cebd4961c22
Matias Valladares <matias.valladares@gmail.com>
2022-07-23 19:09:38 -0400

    primer commit

---
+++ evaluacion1/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function getById(){}
+	public function getAll(){}
+	public function create(){}
+	public function delete(){}
+	public function update(){}
+}
