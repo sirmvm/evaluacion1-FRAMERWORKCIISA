65f358a4836d828503d52d02d20bfb84016980db
pablogutsan <pablogutierrez@live.cl>
2022-07-24 02:43:32 -0400

    Se actualizan algunas etiquetas

--- evaluacion1/resources/views/plantilla.blade.php
+++ evaluacion1/resources/views/plantilla.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Automotora</title>
+  <title>Admin-Store</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -49,7 +49,7 @@
 
     @include('modulos.menu')
 
-    @include('modulos.automoviles')
+    @include('modulos.productos')
 
     <footer class="main-footer">
       <div class="pull-right">
