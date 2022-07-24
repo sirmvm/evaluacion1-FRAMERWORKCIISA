9a7b37cc20aabd7d07dbeb5eeb1f305dc0bdc11a
pablogutsan <pablogutierrez@live.cl>
2022-07-24 16:28:36 -0400

    segunda actualización de rutas de estilos y script

--- evaluacion1/resources/views/plantilla.blade.php
+++ evaluacion1/resources/views/plantilla.blade.php
@@ -7,28 +7,28 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
   <!-- Morris chart -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/morris.js/morris.css">
+  <link rel="stylesheet" href="bower_components/morris.js/morris.css">
   <!-- jvectormap -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jvectormap/jquery-jvectormap.css">
+  <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
   <!-- Date Picker -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
   
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   
-  <link rel="stylesheet" href="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
+  <link rel="stylesheet" href="bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -68,43 +68,43 @@
 
 
 <!-- jQuery 3 -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jquery-ui/jquery-ui.min.js"></script>
+<script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/ower_components/raphael/raphael.min.js"></script>
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/morris.js/morris.min.js"></script>
+<script src="bower_components/raphael/raphael.min.js"></script>
+<script src="bower_components/morris.js/morris.min.js"></script>
 <!-- Sparkline -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/moment/min/moment.min.js"></script>
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="bower_components/moment/min/moment.min.js"></script>
+<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
 <!-- datepicker -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
 <!-- Slimscroll -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/dist/js/adminlte.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/dist/js/pages/dashboard.js"></script>
+<script src="dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/dist/js/demo.js"></script>
+<script src="dist/js/demo.js"></script>
 
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
 
-<script src="http://localhost/evaluacion1-FRAMERWORKCIISA/evaluacion1/public/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
